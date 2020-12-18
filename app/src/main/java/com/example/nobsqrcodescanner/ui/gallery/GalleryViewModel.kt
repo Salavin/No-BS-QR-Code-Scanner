102c34ad5d8b7d815acf7d355ed1d15844f1fb91
Salavin <zeldatwili1@gmail.com>
2020-12-18 15:49:36 -0600

    Initial Commit.

---
+++ app/src/main/java/com/example/nobsqrcodescanner/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.nobsqrcodescanner.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel()
+{
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
